e69aa96e80840292c8f5b83a788817f925081082
Emeka Elo-Chukwuma <elochux@gmail.com>
2022-03-10 09:26:37 +0100

    feat: added smart contract

---
+++ hello_world.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.12;
+
+contract HelloWorld {
+    string public greeting;
+
+    constructor() {
+        greeting = "Hello World";
+    }
+
+    function setGreeting(string memory _newGreeting) public {
+        greeting = _newGreeting;
+    }
+
+    function greet() public view returns (string memory) {
+        return greeting;
+    }
+}
